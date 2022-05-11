3557eb092b5d58ea33430ce59631fc7067b492fb
James Campbell <jcampbell92@icloud.com>
2022-05-11 14:27:40 -0400

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.all.destroy
+
+
+Restaurant.create({name: "Joe Beef", category: "french", address: "verdun", phone_number: "555555555"})
+Restaurant.create({name: "Garde Manger", category: "french", address: "old port", phone_number: "1805555555"})
+Restaurant.create({name: "Dominos", category: "italian", address: "everywhere", phone_number: "39234324"})
+Restaurant.create({name: "Rosedale Diner", category: "french", address: "toronto", phone_number: "4160343043"})
+Restaurant.create({name: "Sotto Sotto", category: "french", address: "toronto", phone_number: "4169343043"})
